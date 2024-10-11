535c060db0c2155194def9c0444bea66a1ef7d2a
AC-Baleni <baleniicaviwe@gmail.com>
2024-10-12 00:02:47 +0200

    backend: The first route for adding users to the database when they signup

--- src/heardaware/backend/BackendService.java
+++ src/heardaware/backend/BackendService.java
@@ -1,9 +1,73 @@
 package heardaware.backend;
 
+import blazing.BlazingLog;
+import blazing.BlazingResponse;
+import blazing.Initializer;
+import blazing.Post;
+import blazing.Static;
+import blazing.WebServer;
+import java.sql.*;
+import blazing.crypto.HashUtils;
+import blazing.json.JSon;
+import java.util.HashMap;
+
 /**
  *
  * @author School EC
  */
+@WebServer
 public class BackendService {
-	
+
+	private static Connection conn;
+
+	@Initializer
+	public static void init() {
+		String url = "jdbc:sqlite:db/packages.db";
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException ex) {
+			BlazingLog.panic(ex.getMessage());
+		}
+	}
+
+	@Post("/v1/users/signup")
+	public static void signup(BlazingResponse response) {
+		var params = response.params();
+
+		String username = params.get("username");
+		String company_name = params.get("company_name");
+		String email = params.get("email");
+		String password = params.get("password");
+		String password_hash = HashUtils.hash(password).unwrap();
+
+		String sql
+			= """
+ INSERT 
+ 	INTO Users (username, company_name, email, password) 
+ 	VALUES (?, ?, ?, ?)
+  """;
+
+		HashMap<String, Object> map = new HashMap<>();
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, username);
+			stmt.setString(2, company_name);
+			stmt.setString(3, email);
+			stmt.setString(4, password_hash);
+			int rows = stmt.executeUpdate();
+			if (rows != 1) {
+				map.put("status", 500);
+				map.put("message", "Failed to insert user");
+			} else {
+				map.put("status", 200);
+				map.put("message", "User signup successful");
+			}
+		} catch (SQLException ex) {
+			map.put("status", 404);
+			map.put("message", "Fatal: " + ex.getMessage());
+		} finally {
+			var json = JSon.from(map).unwrap();
+			response.sendResponse(json);
+		}
+	}
+
 }
