b8dea03c31de28758646e667b4b34609bb2c46ba
AC-Baleni <baleniicaviwe@gmail.com>
2024-10-12 01:37:54 +0200

    db: The actual SQL script

--- db/users.sql
+++ db/users.sql
@@ -1,8 +1,15 @@
 
 CREATE TABLE IF NOT EXISTS Users(
 	uuid INTEGER PRIMARY KEY, 
-	username VARCHAR(9) NOT NULL, 
 	company_name TEXT NOT NULL, 
 	email TEXT NOT NULL, 
-	password VARCHAR(15) NOT NULL
+	password TEXT NOT NULL
 );
+
+
+INSERT 
+	INTO Users (company_name, email, password) 
+	VALUES ('herdaware', 'admin@herdaware.com', 'admin');
+	
+--SELECT username FROM Users WHERE username = ?
+
